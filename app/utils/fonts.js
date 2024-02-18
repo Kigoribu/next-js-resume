6a8978a1fcbf54aaad6bac5edff02b5b52246107
Kirill <kirillgorbunov1999@gmail.com>
2024-02-19 00:01:32 +0300

    fix: fonts not load

--- app/utils/fonts.js
+++ app/utils/fonts.js
@@ -7,7 +7,5 @@ export const kaushan_script = Kaushan_Script({
 
 export const geologica = Geologica({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
-  display: "swap",
-  adjustFontFallback: false,
   subsets: ["cyrillic"],
 });
