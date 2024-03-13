c8b6c9e46878be8a2c130194ea3fafc6de6a4f13
Kirill <kirillgorbunov1999@gmail.com>
2024-03-13 16:57:39 +0300

    refactor: change types in getChildrenByType module

--- lib/getChildrenByType.tsx
+++ lib/getChildrenByType.tsx
@@ -1,27 +1,14 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-/**
- * Gets the string type of the component or core html (JSX) element. React Fragments will return type 'react.fragment'. Priority will be given to the prop '__TYPE'.
- *
- * @param {ReactNode} component - The component to type check
- * @returns {string} - The string representation of the type
- */
-export const typeOfComponent = (component: any) =>
-  component?.type?.displayName ||
-  component?.type?.toString().replace("Symbol(react.fragment)", "react.fragment") ||
-  undefined;
+export const typeOfComponent = (component: any): string => {
+  return (
+    component?.type?.displayName ||
+    component?.type?.toString().replace("Symbol(react.fragment)", "react.fragment") ||
+    undefined
+  );
+};
 
-/**
- * Gets all children by specified type. This function will check the prop '__TYPE' first and then the 'type' string to match core html elements. To find a React Fragment, search for type 'react.fragment'.
- *
- * @param {ReactNode} children - JSX children
- * @param {string[]} types - Types of children to match
- * @returns {ReactNode[]} - Array of matching children
- * @example
- * // Finds all occurrences of ToDo (custom component), div, and React Fragment
- * getChildrenByType(children, ['ToDo', 'div', 'react.fragment']);
- */
-export const getChildrenByType = (children: any, types: any) => {
+export const getChildrenByType = (children: ReactNode, types: string[]): ReactNode[] => {
   return React.Children.toArray(children).filter(
     (child) => types.indexOf(typeOfComponent(child)) !== -1
   );
