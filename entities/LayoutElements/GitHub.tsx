4c0710801fe07a30fd6f60e660e4e5c2e4266e08
Kirill <kirillgorbunov1999@gmail.com>
2024-03-15 02:58:17 +0300

    fix: mobile github link doesn't work

--- entities/LayoutElements/GitHub.tsx
+++ entities/LayoutElements/GitHub.tsx
@@ -4,7 +4,7 @@ import { FaGithubAlt } from "react-icons/fa";
 export const GitHub = () => {
   return (
     <a
-      className="fixed max-md:right-12 max-md:top-12 md:bottom-12 md:left-12 text-white hover:text-cyan-200 cursor-pointer"
+      className="fixed max-md:right-12 max-md:top-10 md:bottom-12 md:left-12 text-white hover:text-cyan-200 cursor-pointer z-50"
       href="https://github.com/Kigoribu"
       target="_blank"
       rel="noopener noreferrer"
